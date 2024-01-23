f84c885bdeccbfef3fb5411a124ecc18f4a0770e
Bambou72 <82939192+Bambou72@users.noreply.github.com>
2024-01-23 12:43:39 +0100

    Add theme system

--- Editor/Widget/Settings.cs
+++ Editor/Widget/Settings.cs
@@ -1,4 +1,7 @@
-﻿using ImGuiNET;
+﻿using Gyrfalcon.Debugs;
+using GyrfalconToolKit.Editor.Utils;
+using ImGuiNET;
+using Newtonsoft.Json;
 using OpenTK.Mathematics;
 using System;
 using System.Collections.Generic;
@@ -14,9 +17,42 @@ namespace GyrfalconToolKit.Editor.Widget
         Themes,
         Others
     }
+    [Serializable]
+    internal class ThemeSave
+    {
+        public string SelectedTheme;
+    }
     internal static class Settings
     {
         static PreferencesType PrefType = PreferencesType.Themes;
+        static List<Theme> Themes = new();
+        static ThemeSave Themesave;
+        internal static void LoadThemes()
+        {
+            Themes.Clear();
+            string[] themes = Directory.GetFiles("Themes", "*.json");
+            foreach(string theme in themes)
+            {
+                Themes.Add(JsonConvert.DeserializeObject<Theme>(File.ReadAllText(theme)));
+            }
+
+        }
+        internal static void LoadThemesSave()
+        {
+            Themesave = JsonConvert.DeserializeObject<ThemeSave>(File.ReadAllText("Themes/Theme.thc"));
+            foreach(Theme theme in Themes)
+            {
+                if (theme.Name== Themesave.SelectedTheme)
+                {
+                    theme.LoadTheme(ImGui.GetStyle());
+                }
+            }
+        }
+        internal static void SaveThemesSave()
+        {
+            string Json = JsonConvert.SerializeObject(Themesave);
+            File.WriteAllText( "Themes/Theme.thc",Json);
+        }
         internal static void ShowSettings()
         {
             if (EditorState.Settings)
@@ -38,6 +74,19 @@ namespace GyrfalconToolKit.Editor.Widget
                     if(PrefType == PreferencesType.Themes)
                     {
                         ImGui.Text("Themes");
+                        if(ImGui.Button($"{FontAwesome6.Repeat}"))
+                        {
+                            LoadThemes();
+                        }
+                        foreach(Theme theme in Themes)
+                        {
+                            if(ImGui.Selectable(theme.Name))
+                            {
+                                theme.LoadTheme(ImGui.GetStyle());
+                                Themesave.SelectedTheme = theme.Name;
+                                SaveThemesSave();
+                            }
+                        }
                     }
                     ImGui.End();
                 }
